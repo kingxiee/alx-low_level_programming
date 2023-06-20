34265111629c66ab4aebf24a6ffabe467dd988a4
kingxiee <ominyik@yahoo.co.uk>
2023-06-20 11:39:04 -0700

    Nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,14 @@
 int main(void)
 {
 	_putchar('_');
-	_putchar('P');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('.');
-	_putchar('c');
+	_putchar('\n');
 
 	return (0);
 }
