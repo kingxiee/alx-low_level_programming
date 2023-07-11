7f96be26cdca4cf53b87e1dd719447d6176886fd
kingxiee <ominyik@yahoo.co.uk>
2023-07-11 05:22:10 -0700

    My maloc file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,24 +10,25 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int i, j;
-    char *str;
-    for (i = 0; s1 != NULL && *(s1 + i) != '\0'; i++)
-        ;
-    for (j = 0; s2 != NULL && *(s2 + j) != '\0'; j++)
-        ;
-    str = malloc(sizeof(char) * i + j + 1);
-    if (str)
-    {
-        for (i = 0; s1 != NULL && *(s1 + i) != '\0'; i++)
-            *(str + i) = *(s1 + i);
-        for (j = 0; s2 != NULL && *(s2 + j) != '\0'; j++)
-            *(str + i + j) = *(s2 + j);
-        *(str + i + j) = '\0';
-        return (str);
-    }
-    else
-    {
-        return (NULL);
-    }
+	int i, j;
+	char *str;
+
+	for (i = 0; s1 != NULL && *(s1 + i) != '\0'; i++)
+		;
+	for (j = 0; s2 != NULL && *(s2 + j) != '\0'; j++)
+		;
+	str = malloc(sizeof(char) * i + j + 1);
+	if (str)
+	{
+		for (i = 0; s1 != NULL && *(s1 + i) != '\0'; i++)
+			*(str + i) = *(s1 + i);
+		for (j = 0; s2 != NULL && *(s2 + j) != '\0'; j++)
+			*(str + i + j) = *(s2 + j);
+		*(str + i + j) = '\0';
+		return (str);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
