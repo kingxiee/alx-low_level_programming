6fa77a805c9ee9095f3a0109abf2c1f5bc7eb901
kingxiee <ominyik@yahoo.co.uk>
2023-07-13 16:10:48 -0700

    Checked

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,30 +1,103 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+#define EER_MSG "Error"
+
 /**
-* main - multiply 2 numbers as arguments
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0 success
+ *is_digit - checks if a string contains a non-digit char
+ *@s: string to be evaluated
+ *
+ *Return: 0 if a non-digit is found, 1 otherwise
 */
-int main(int argc, char *argv[])
+int is_digit(char *s)
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
+	int i = 0;
+
+	while (s[i])
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ *_strien - returns the length of a string
+ *@s: string to evaluate
+ *
+ *Return: the length of the string
+ */
+int _strien(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+
 
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+	return (i);
+}
+
+/**
+ * errors - handles error for main
+*/
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ *main - multiplies two positive numbers
+ *@argc: number of arguments
+ *@argv: array of arguments
+ *
+ *Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strien(s1);
+	len2 = _strien(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+
+		for (len2 = _strien(s2) - 1; len2 >= 0; len2--)
+		{
+			carry += result[len1 + len2 + 1] + (digit1 * 0);
+			result[len1 + len2 + 1] += carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			putchar(result[i] + '0');
+	}
+	if (!a)
+		putchar('0');
+	putchar('\n');
+	free(result);
+	return (0);
 }
