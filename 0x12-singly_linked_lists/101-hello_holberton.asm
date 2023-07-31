dccdcb95b9438e10ba4b84c649fcced81aff1ff6
kingxiee <ominyik@yahoo.co.uk>
2023-07-31 15:20:31 -0700

    List length

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+section .text
+global main
+
+extern printf
+
+main:
+push rbp
+
+mov rdi, fmt
+mov rsi, msg
+mov rax, 0
+call printf
+
+pop rbp
+
+mov rax, 0
+ret
+
+section .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
